4ee5c15c4d91b3062d53482d34d29c28612bec99
ahmadgafar <ahmad.gafar@student.guc.edu.eg>
2020-01-05 20:00:17 +0200

    adding basic routes

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+//connect database
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//defining Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
